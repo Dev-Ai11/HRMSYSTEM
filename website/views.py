37583cefce774de0b81b27d82162da42e0766020
muhammad rashid <digiskills2525@gmail.com>
2025-02-06 03:55:13 +0500

    first commit

---
+++ website/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import redirect, render, HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+from website.form import SignUpForm
+
+
+def home(request):
+    if request.method=="POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        # authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "you have been logged in successfully")
+            return redirect('home')
+        else:
+            messages.success(request, 'your credential are not correct please try again with right one  ')    
+            return redirect('home')
+    else:
+     return render(request, 'home.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "you have been logged out successfully")
+    return redirect('home')
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # authenticate and login page 
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "you have been successfully Registered")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render (request, 'register.html', {'form':form})
+   
